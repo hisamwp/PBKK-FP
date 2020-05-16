65f3b666bdc284b2bd15663b0e00a425b4fb9366
hisamwp <36986744+hisamwp@users.noreply.github.com>
2020-05-16 08:41:48 +0700

    uplaod

---
+++ databases.sql
@@ -0,0 +1,97 @@
+/*
+SQLyog Ultimate v9.50 
+MySQL - 5.5.16 : Database - dbci316
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dbci316` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `db_toko`;
+
+/*Table structure for table `tbl_detail_order` */
+
+DROP TABLE IF EXISTS `tbl_detail_order`;
+
+CREATE TABLE `tbl_detail_order` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `order_id` int(10) DEFAULT NULL,
+  `produk` int(10) DEFAULT NULL,
+  `qty` int(10) DEFAULT NULL,
+  `harga` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tbl_detail_order` */
+
+/*Table structure for table `tbl_kategori` */
+
+DROP TABLE IF EXISTS `tbl_kategori`;
+
+CREATE TABLE `tbl_kategori` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nama_kategori` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tbl_kategori` */
+
+insert  into `tbl_kategori`(`id`,`nama_kategori`) values (1,'Laptop'),(2,'Smartphone'),(3,'Robot');
+
+/*Table structure for table `tbl_order` */
+
+DROP TABLE IF EXISTS `tbl_order`;
+
+CREATE TABLE `tbl_order` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `tanggal` date DEFAULT NULL,
+  `pelanggan` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tbl_order` */
+
+/*Table structure for table `tbl_pelanggan` */
+
+DROP TABLE IF EXISTS `tbl_pelanggan`;
+
+CREATE TABLE `tbl_pelanggan` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `alamat` varchar(50) DEFAULT NULL,
+  `telp` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tbl_pelanggan` */
+
+/*Table structure for table `tbl_produk` */
+
+DROP TABLE IF EXISTS `tbl_produk`;
+
+CREATE TABLE `tbl_produk` (
+  `id_produk` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nama_produk` varchar(50) DEFAULT NULL,
+  `deskripsi` varchar(50) DEFAULT NULL,
+  `harga` varchar(10) DEFAULT NULL,
+  `gambar` varchar(50) DEFAULT NULL,
+  `kategori` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id_produk`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tbl_produk` */
+
+insert  into `tbl_produk`(`id_produk`,`nama_produk`,`deskripsi`,`harga`,`gambar`,`kategori`) values (1,'Laptop 1','Lorem ipsum dolor sit amet, consectetur adipisicin','3500000','laptop1.jpg',1),(2,'Laptop 2','Lorem ipsum dolor sit amet, consectetur adipisicin','6250000','laptop2.jpg',1),(3,'Laptop 3','Lorem ipsum dolor sit amet, consectetur adipisicin','7250000','laptop3.jpg',1),(4,'Smartphone 1','Lorem ipsum dolor sit amet, consectetur adipisicin','3560000','hp1.jpg',2),(5,'Smartphone 2','Lorem ipsum dolor sit amet, consectetur adipisicin','4300000','hp2.jpg',2),(6,'Smartphone 3','Lorem ipsum dolor sit amet, consectetur adipisicin','5100000','hp3.jpg',2),(7,'Robot 1','Lorem ipsum dolor sit amet, consectetur adipisicin','3500000','robot1.jpg',3),(8,'Robot 2','Lorem ipsum dolor sit amet, consectetur adipisicin','4500000','robot2.jpg',3),(9,'Robot 3','Lorem ipsum dolor sit amet, consectetur adipisicin','6100000','robot3.jpg',3);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
